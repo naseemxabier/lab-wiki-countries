ca8f0734b99c76636520cd0c42c94f1cf9ebad42
Xabier Naseem <naseemxabier@gmail.com>
2023-03-03 11:33:08 +0100

    done

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react"
 import axios from "axios"
+
 function CountryDetails({ countries }) {
 
     const { countryName } = useParams()
@@ -9,47 +10,51 @@ function CountryDetails({ countries }) {
     const country = countries.find((c) => c.alpha3Code === countryName);
     const borderCountries = countries.filter((c) => country.borders.includes(c.alpha3Code));
     const [paises, setPaises] = useState(countries)
-    useEffect (()=> {
-   
-        axios.get(`https://ih-countries-api.herokuapp.com/countries/${paises.alpha3Code}`)
-        .then (response => {
-          setPaises(response.data)
-        })
-        .catch(err => console.log(err))
-    })
-    
+    console.log(countryName)
+    useEffect(() => {
+
+        axios.get(`https://ih-countries-api.herokuapp.com/countries/${countryName}`)
+            .then(response => {
+                console.log("HOLAA")
+                setPaises(response.data)
+                
+            })
+            .catch(err => console.log(err))
+    }, [])  
+
+
     return (
 
 
-                <div class="col-7">
-                <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt="imagen" />
-                    <h1>{currentCountry.name.common}</h1>
-                    <table class="table">
-                        <thead></thead>
-                        <tbody>
-                            <tr>
-                                <td style={{ width: '30%' }}>Capital</td>
-                                <td>{currentCountry.capital}</td>
-                            </tr>
-                            <tr>
-                                <td>Area</td>
-                                <td>
-                                    {currentCountry.area} km
-                                    <sup>2</sup>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Borders</td>
-                                <td>
-                                    <ul>
-                                        {borderCountries.map ((border => (<li><Link to = {"/"+border.alpha3Code}>{border.name.common}</Link></li>)))}
-                                        
-                                    </ul>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+        <div className="col-7">
+            <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt="imagen" />
+            <h1>{currentCountry.name.common}</h1>
+            <table className="table">
+                <thead></thead>
+                <tbody>
+                    <tr>
+                        <td style={{ width: '30%' }}>Capital</td>
+                        <td>{currentCountry.capital}</td>
+                    </tr>
+                    <tr>
+                        <td>Area</td>
+                        <td>
+                            {currentCountry.area} km
+                            <sup>2</sup>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Borders</td>
+                        <td>
+                            <ul>
+                                {borderCountries.map((border => (<li><Link to={"/" + border.alpha3Code}>{border.name.common}</Link></li>)))}
+
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
 
     )
